77a5b51c20e6b96f200fd8e7b74a1f94844d670e
buonoalex <loopatthecity@gmail.com>
2023-11-30 16:50:36 +0100

    Finish work and apply Override on equals

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,6 +1,6 @@
 package com.ferragnez.party;
 
-import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class CheckGuest {
@@ -8,7 +8,7 @@ public class CheckGuest {
         //Apro scan
         Scanner myScanBro = new Scanner(System.in);
 
-        Guess[] guessList = new Guess[10];
+        Guess[] guessList = new Guess[11];
 
         //Completamento array da stringa
         String listCompleteGuess = "Dua,Lipa,Paris,Hilton,Manuel,Agnelli,Alessandro,Aleotti,Francesco,Totti,Ilary,Blasi,Bebe,Vio,Sergio,Lerme,Pardis,Zarei,Martina,Maccherone,Rachel,Zeilic";
@@ -17,24 +17,49 @@ public class CheckGuest {
         //Riempimemto
         String[] arrayGuess = listCompleteGuess.split(",");
 
-        boolean sentinella = false;
-        for (int i = 0; i < guessList.length && !sentinella; i++) {
+        for (int i = 0, j = 0; i < guessList.length; i++, j = j + 2) {
             guessList[i] = new Guess();
-            for (int j = 0; j < (guessList.length*2)+1 ; j=j+2) {
-                guessList[i].setName(arrayGuess[j]);
-                guessList[i].setSurname(arrayGuess[j+1]);
-                guessList[i].PrintGuess();
-            }
-            sentinella = true;
+            guessList[i].setName(arrayGuess[j]);
+            guessList[i].setSurname(arrayGuess[j + 1]);
         }
 
+        //Stampo Array
+        System.out.println("----------------------");
+        System.out.println("Ecco la Lista degli ospiti che posso entrare : \n");
+
+        for (int i = 0; i < guessList.length; i++) {
+            guessList[i].PrintGuess();
+        }
+
+        System.out.println("-----------------------");
+
         //Input Utente
+        Guess guessToFind = new Guess();
+        String nameGuess;
+        System.out.println("Inserisci il nome del tuo ospite : ");
+        guessToFind.setName(myScanBro.nextLine());
 
+        String surnameGuess;
+        System.out.println("Inserisci il cognome del tuo ospite : ");
+        guessToFind.setSurname(myScanBro.nextLine());
 
+        boolean sentinella = false;
+        for (int i = 0; i < guessList.length && !sentinella ; i++) {
+            if (guessToFind.equals(guessList[i])){
+                sentinella = true;
+            }
+        }
 
+        if (sentinella){
+            System.out.println("PREGO puoi entrare");
+        }else {
+            System.out.println("NON puoi entrare");
+        }
 
 
         //Chiudo scan
         myScanBro.close();
     }
+
 }
+
--- src/com/ferragnez/party/Guess.java
+++ src/com/ferragnez/party/Guess.java
@@ -20,6 +20,30 @@ public class Guess {
         System.out.println("Name : "+this.name+" Surname : "+this.surname);
     }
 
+    //Metodo equals
+    @Override
+    public boolean equals(Object oggetto) {
+        Guess guess;
+        if (oggetto instanceof Guess){
+            guess = (Guess) oggetto;
+        }else {
+            return false;
+        }
+        if (this == guess){
+            return true;
+        }
+        //Add control
+        String parolaName1 = this.name.toLowerCase();
+        String parolaName2 = guess.getName().toLowerCase();
+        String parolaSurname1 = this.surname.toLowerCase();
+        String parolaSurname2 = guess.surname.toLowerCase();
+        if (parolaName1.equals(parolaName2) && parolaSurname1.equals((parolaSurname2))){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
     //Get e Set
     public String getName() {
         return name;
